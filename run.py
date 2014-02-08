1d05a4368040de31169f3ac142f2784111f1a743
John <jctanner@interchange.ubc.ca>
2014-02-08 11:45:49 -0800

    API has new getJobs(userId) function, bug fixes in API and implemented Scheduler daemon, jobs2Schedules.

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 import time
 import string
 import subprocess
+import os
 
 def preprocessJob (job):
 	return string.joinfields(job.split(' ')[5:], ' ')
@@ -18,7 +19,7 @@ jobs = CronTab(tabfile='crontab.tab')
 count = len(jobs)
 schedules = [job.schedule(date_from=datetime.now()) for job in jobs]
 jobs = map(str, jobs)
-ft_ext = { 'sh':'bash', 'py':'python', 'pl':'perl', '':'' } #supports bash, python, perl, and single commands
+ft_ext = { 'sh':'bash', 'py':'python', 'pl':'perl', '':'' }				#supports bash, python, perl, and single commands
 cmds = []
 for job in jobs:
 	job = string.joinfields(job.split(' ')[5:], ' ')
@@ -28,16 +29,43 @@ for job in jobs:
 	cmds.append("%s %s" % (typ, job))
 nxt = [schedule.get_next() for schedule in schedules]
 
+
+def sortSchedules (schedules):
+
+	return schedules.sort(key=lambda schedule: schedule.timeToRun, reverse=False)
+
+
+def jobs2Schedules (jobs):
+
+	schedules = []
+
+	while (len(jobs) > 0)):
+		
+		job = jobs.pop()
+		
+		cmd = CronTab(tab="%s %s" % (job.interval, job.command)			#these two lines
+		cmd_sch = cmd.getSchedule(date_from=datetime.now())				#allow us to obtain next timeToRun
+		
+		schedule = api.schedule(cmd_sch.get_next, job, worker=None, completedTime=None)
+		
+	 	schedules.append(schedule)
+
+	 return schedules
+
+
 while True:
-	gaps = []
-	for i in range(count):
-		delta = nxt[i] - datetime.now()
-		gap = delta.total_seconds()
-		gaps.append(gap)
-		#Could probably map this so it's faster
-		if abs(gap) < 1:
-			subprocess.call(cmds[i], shell=True) #this gets replaced
-			nxt[i] = schedules[i].get_next()
-		elif gap < 0:
-			nxt[i] = schedules[i].get_next()
-	time.sleep(int(min(gaps))) #so your processor doesn't explode
+
+	workers = []
+
+	schedules = sortSchedules(jobs2Schedules)
+
+	workers = getWorkers()
+	
+	worker = workers.pop()
+	
+	for schedule in schedules:
+
+		schedule.worker = worker
+
+ 	time.sleep(3600)													#so your processor doesn't explode
+
