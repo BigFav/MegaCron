2deb95ffdf47b8a815ef9cd35676ab5c6ffad097
Favian Contreras <fnc4@cornell.edu>
2014-02-07 22:08:48 -0500

    There UI should be good. There are placeholder functions in it as of now though (the schedule creator, and the DATABASE calls).

---
+++ userInterface.py
@@ -0,0 +1,32 @@
+from crontab import CronTab
+import string
+import os
+import subprocess
+
+uid = os.getuid()
+schedules = DATABASE.get(uid)
+tbfile = "crontab.tab"
+with open(tbfile, 'w') as tab:
+	for schedule in schedules:
+		tab.write("%s %s\n" % (schedule.interval, schedule.command))
+
+editor = os.getenv('EDITOR')
+if editor:
+	os.system("%s %s" % (editor, tbfile))
+else:
+	subprocess.call("vim %s" % tbfile, shell=True)
+
+jobs = CronTab(tabfile=tbfile)
+jobs = map(str, jobs)
+schedules = []
+for job in jobs:
+	tmp = job.split(' ')
+	interval = string.joinfields(tmp[:5], ' ')
+	cmd = string.joinfields(tmp[5:], ' ')
+	schedule = schedule()
+	schedule.interval = interval
+	schedule.command = cmd
+	schedules.append(schedule)
+
+DATABASE.setSchedules(schedules, uid)
+
