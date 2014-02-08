d71d7d9bda90cc95995cd733b8cb7837bf4928bb
John <jctanner@interchange.ubc.ca>
2014-02-08 12:18:54 -0800

    Added api library import and calls.

--- run.py
+++ run.py
@@ -3,32 +3,7 @@ from croniter import croniter
 from datetime import datetime
 from datetime import timedelta
 import time
-import string
-import subprocess
-import os
-
-def preprocessJob (job):
-	return string.joinfields(job.split(' ')[5:], ' ')
-
-def preprocessExt (job, ft_ext):
-	return ft_ext[job.split('/')[-1].partition('.')[2][:2]]
-
-
-jobs = CronTab(tabfile='crontab.tab')
-#check if jobs are valid!!!!!
-count = len(jobs)
-schedules = [job.schedule(date_from=datetime.now()) for job in jobs]
-jobs = map(str, jobs)
-ft_ext = { 'sh':'bash', 'py':'python', 'pl':'perl', '':'' }				#supports bash, python, perl, and single commands
-cmds = []
-for job in jobs:
-	job = string.joinfields(job.split(' ')[5:], ' ')
-	typ = ""
-	if job[:7] != "python ":
-		typ = ft_ext[job.split('/')[-1].partition('.')[2][:2]]
-	cmds.append("%s %s" % (typ, job))
-nxt = [schedule.get_next() for schedule in schedules]
-
+import API
 
 def sortSchedules (schedules):
 
@@ -46,7 +21,7 @@ def jobs2Schedules (jobs):
 		cmd = CronTab(tab="%s %s" % (job.interval, job.command)			#these two lines
 		cmd_sch = cmd.getSchedule(date_from=datetime.now())				#allow us to obtain next timeToRun
 		
-		schedule = api.schedule(cmd_sch.get_next, job, worker=None, completedTime=None)
+		schedule = API.schedule(cmd_sch.get_next, job, worker=None, completedTime=None)
 		
 	 	schedules.append(schedule)
 
@@ -55,17 +30,17 @@ def jobs2Schedules (jobs):
 
 while True:
 
-	workers = []
+	schedules = sortSchedules(jobs2Schedules(API.getJobs()))
 
-	schedules = sortSchedules(jobs2Schedules)
-
-	workers = getWorkers()
+	workers = API.getWorkers()
 	
 	worker = workers.pop()
 	
 	for schedule in schedules:
 
 		schedule.worker = worker
+
+	API.addSchedule(schedules)
 
  	time.sleep(3600)													#so your processor doesn't explode
 
