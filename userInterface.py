1a001af171e785c1bcc1eca72bdaaca3ab13d661
Favian Contreras <fnc4@cornell.edu>
2014-02-08 15:59:40 -0500

    Updated userInterface. Tested, however has a problem with duplication in the dump file.

--- userInterface.py
+++ userInterface.py
@@ -1,20 +1,15 @@
-from crontab import CronTab
-from croniter import croniter
-from datetime import datetime
-from datetime import timedelta
-import time
 import string
-import os
 import subprocess
-from API.py import getJobs
-from API.py import setJobs
+import os
+from crontab import CronTab
+import API
 
 uid = str(os.getuid())
-jobs_c = getJobs(uid)
+jobs_old = API.getJobs(uid)
 tbfile = "crontab.tab"
 with open(tbfile, 'w') as tab:
-	for schedule in schedules:
-		tab.write("%s %s\n" % (schedule.interval, schedule.command))
+	for job in jobs_old:
+		tab.write("%s %s\n" % (job.interval, job.command))
 
 editor = os.getenv('EDITOR')
 if editor:
@@ -23,13 +18,13 @@ else:
 	subprocess.call("vim %s" % tbfile, shell=True)
 
 
-jobs_c = CronTab(tabfile=tbfile)
-jobs_c = map(str, jobs_c)
-jobs = []
-for job in jobs_c:
+jobs_old = CronTab(tabfile=tbfile)
+jobs_old = map(str, jobs_old)
+jobs_new = []
+for job in jobs_old:
 	tmp = job.split(' ')
 	interval = string.joinfields(tmp[:5], ' ')
 	cmd = string.joinfields(tmp[5:], ' ')
-	jobs.append(Job(interval, cmd, uid))
+	jobs_new.append(API.Job(interval, cmd, uid))
 
-setJobs(jobs)
+API.setJobs(jobs_new)
