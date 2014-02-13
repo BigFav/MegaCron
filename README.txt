e1aa5891997b4ec6491d7d7cbc94050a97b8388e
John <jctanner@interchange.ubc.ca>
2014-02-12 17:55:13 -0800

    Changes were untracked, folder structure for release should be fine.

---
+++ README.txt
@@ -0,0 +1,10 @@
+Wikimedia Distributed Cron
+________________________________________________________________________
+
+crontab.py - gets and sets user's crontab
+
+schedule.py - orders, schedules, and divies commands
+
+worker.py - runs instances of commands
+
+API.py - the API for all interactions; no external dependencies
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Megacron',
+    version='0.1',
+    description='Distributed Cron Replacement',
+    author='John Tanner',
+    author_email='jctanner@interchange.ubc.ca',
+    packages=['API','crontab','scheduler','status','worker'],
+    url='https://www.mediawiki.org/wiki/Facebook_Open_Academy/Cron',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
