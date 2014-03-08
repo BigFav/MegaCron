46edd50fea5c6c42a11690cecd8a33699a8fb880
John <jctanner@interchange.ubc.ca>
2014-03-07 23:38:39 -0800

    Changed url in setup.py.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     description='Distributed Cron Replacement',
     author='John Tanner, Favian Contreras, Ben Zhegers, Ben Zeghers',
     author_email='megacronteam@gmail.com',
-    packages=['API', 'edit', 'scheduler', 'status', 'worker'],
-    url='https://www.mediawiki.org/wiki/Facebook_Open_Academy/Cron',
+    packages=['API','edit','scheduler','status','worker'],
+    url='https://pypi.python.org/pypi/Megacron',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
