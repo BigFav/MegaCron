88fa179f44267514f342a161f2b7109b2bd87a00
umzegher <umzegher@myumanitoba.ca>
2014-03-10 01:13:59 -0500

    Add scripts to package

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ setup(
     author='John Tanner, Favian Contreras, Ben Zeghers',
     author_email='megacronteam@gmail.com',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'megacrond = megacron.daemon.main:main',
+            'megacrontab = megacron.edit:main',
+            'megacron-status = megacron.status:main'
+        ]
+    },
     url='https://www.mediawiki.org/wiki/Facebook_Open_Academy/Cron',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read()
