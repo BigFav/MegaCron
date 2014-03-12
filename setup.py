9879544d8504770efc8f843ab3532be6c281f22b
John <jctanner@interchange.ubc.ca>
2014-03-11 18:06:35 -0700

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ setup(
     description='Distributed Cron Replacement',
     author='John Tanner, Favian Contreras, Ben Zeghers',
     author_email='megacronteam@gmail.com',
-<<<<<<< HEAD
-    packages=['API','edit','scheduler','status','worker'],
-    url='https://pypi.python.org/pypi/Megacron',
-=======
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -28,7 +24,6 @@ setup(
         ('/etc', ['megacron/conf/megacron.conf'])
     ],
     url='https://www.mediawiki.org/wiki/Facebook_Open_Academy/Cron',
->>>>>>> 7af99e2aa20a339c941e45c90628c902839ebe26
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.rst').read()
 )
