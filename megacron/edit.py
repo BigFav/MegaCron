f0cc2604cc622c1f272c910e7477f8b5f1c32d32
Favian Contreras <fnc4@cornell.edu>
2014-03-12 07:26:10 -0400

    edit.py now checks validity of jobs, prompts user to correct invalid crontabs, and opens the crontab via preferred text editor, if available. Otherwise it prompts user to set a preferred text editor and exits.

--- megacron/edit.py
+++ megacron/edit.py
@@ -1,41 +1,84 @@
+import os
 import sys
 import string
 import subprocess
-import os
 import tempfile
 from datetime import datetime
 
+from crontab import CronTab
 from megacron import api
 
 
-def main():
-    uid = os.getuid()
-
-    if len(sys.argv) < 3:
+def getCrontab(uid, valid_crontab):
+    if (len(sys.argv) < 3) or (valid_crontab == False):
         jobs_old = api.get_jobs_for_user(uid)
-        with tempfile.NamedTemporaryFile('w', delete=False) as temp:
-            for job in jobs_old:
-                temp.write("%s %s\n" % (job.interval, job.command))
+        if valid_crontab == None:
+            with tempfile.NamedTemporaryFile('w', delete=False) as temp:
+                for job in jobs_old:
+                    temp.write("%s %s\n" % (job.interval, job.command))
 
-            tb_file = temp.name
+                tb_file = temp.name
+        else:
+            tb_file = sys.argv[2]
 
+        visual = os.getenv('VISUAL')
         editor = os.getenv('EDITOR')
-        if editor is not None:
+        if visual:
+            os.system("%s %s" % (visual, tb_file))
+        elif editor:
             os.system("%s %s" % (editor, tb_file))
         else:
-            subprocess.call("vim %s" % tb_file, shell=True)
+            try:
+                subprocess.check_call(["vi", str(tb_file)])
+            except OSError:
+                if len(sys.argv) < 3:
+                    os.unlink(tb_file)
+                sys.exit("No text editor available. Please set your VISUAL" + \
+                         " or EDITOR environment variable.")
+
     elif sys.argv[1] == '-u':
         tb_file = sys.argv[2]
 
+    return tb_file
+
+
+def processEdits(uid, tb_file):
     jobs_new = []
     with open(tb_file, 'r') as tab:
-        for job in tab:
-            tmp = job.strip().split(' ')
+        cron = CronTab()
+        for line in tab:
+            tmp = line.strip().split(' ')
             interval = string.joinfields(tmp[:5], ' ')
             cmd = string.joinfields(tmp[5:], ' ')
+            job = cron.new(command=cmd)
+            valid_interval = job.setall(interval)
+            if not job.is_valid() or not valid_interval:
+                # Different syntax in Python 3 'input()'
+                cont = raw_input(
+                       "The crontab you entered has invalid " + \
+                       "entries, would you like to edit it again? (y/n)\n")
+                if cont == 'n':
+                    if len(sys.argv) < 3:
+                        os.unlink(tb_file)
+                    sys.exit(1)
+                elif cont == 'y':
+                    return False
             jobs_new.append(api.Job(interval, cmd, uid, datetime.now()))
 
     if len(sys.argv) < 3:
         os.unlink(tb_file)
 
     api.set_jobs(jobs_new, uid)
+    return True
+
+
+def main():
+    uid = os.getuid()
+    valid_crontab = None
+    while not valid_crontab:
+        tb_file = getCrontab(uid, valid_crontab)
+        valid_crontab = processEdits(uid, tb_file)
+
+
+if __name__ == '__main__':
+    main()
