9ade30100fb8883924f29206fe64e7bfb68cbbad
greg-github <gregmanis@gmail.com>
2014-03-22 00:14:26 -0400

    Update status.py

--- megacron/status.py
+++ megacron/status.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from megacron import api
 
 def get_worker_status():
-    L = len(API.get_workers())
+    L = len(api.get_workers())
     if (L == 1):
         print "1 worker is up"
     else:
         print "%d workers are up" % L
         
 def get_num_jobs():
-    L = len(API.get_jobs())
+    L = len(api.get_jobs())
     if (L == 1):
         print "1 job in queue"
     else:
@@ -20,15 +20,15 @@ def get_num_jobs():
 def get_next_schedule_time():
     now = datetime.now()
     comp = datetime.max
-    for worker in API.get_workers():
-        for schedule in API.get_schedules(worker):
+    for worker in api.get_workers():
+        for schedule in api.get_schedules(worker):
             if (schedule.time_to_run < comp and schedule.time_to_run > now):
                 comp = schedule.time_to_run
     print "The next job will be ran at " + comp.strftime("%d %B %Y %I:%M%p")
     
 def get_num_users():
     users = set()
-    for job in API.get_jobs():
+    for job in api.get_jobs():
         users.add(jobs.user_id)
     L = len(users)
     if (L == 1):
@@ -38,8 +38,8 @@ def get_num_users():
 
 def get_num_schedules():
     sum = 0
-    for worker in API.get_workers():
-        sum += len(API.get_schedules(worker))
+    for worker in api.get_workers():
+        sum += len(api.get_schedules(worker))
     if (sum == 1):
         print "There is 1 schedule"
     else:
