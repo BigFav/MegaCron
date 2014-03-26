5a210f68ae71e900650ad96231e1eb108e8b9b4c
John <jctanner@interchange.ubc.ca>
2014-03-26 13:45:04 -0700

    Added first worker unit tests and check_worker_fields function for infrastructure.

--- tests/create_test_jobs.py
+++ tests/create_test_jobs.py
@@ -10,7 +10,7 @@ from megacron import api
 TAB_FILE = './test.tab'
 
 uid = os.getuid()
-uids = { 'uid1': uid+1, 'uid2': uid+2, 'uid3':uid+3 }
+uids = { 'uid1': uid+1, 'uid2': uid+2, 'uid3':uid+3, 'uid4':uid+4 }
 
 def cleanup():
     if(os.access(TAB_FILE, os.F_OK)):
@@ -18,7 +18,7 @@ def cleanup():
     if(os.access(api.FILE_NAME, os.F_OK)):
         os.remove(api.FILE_NAME)
 
-def test_job_fields(self, test_jobs, uid, jobs_list):
+def check_job_fields(self, test_jobs, uid, jobs_list):
     current = 0
     for job in jobs_list:
         self.assertEqual(job.interval, test_jobs[current].interval)
@@ -27,7 +27,13 @@ def test_job_fields(self, test_jobs, uid, jobs_list):
         self.assertEqual(job.last_time_run, test_jobs[current].last_time_run)
         self.assertEqual(job._id, test_jobs[current]._id)
         current += 1
-    return current
+
+def check_worker_fields(self, checkpoint1, checkpoint2, workers_list):
+    current = 1
+    for worker in workers_list:
+        self.assertTrue(worker.heartbeat > checkpoint1 and worker.heartbeat < checkpoint2)
+        self.assertEqual(worker._id, current)
+        current += 1
 
 def create_test_tab(num_of_jobs, uid):
     cron_strings = {}
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,18 +2,97 @@
 
 import unittest
 import os
+import random
 
 from datetime import datetime
 
-from create_test_jobs import create_test_tab, cleanup, test_job_fields, uids
+from create_test_jobs import create_test_tab, cleanup, check_job_fields, check_worker_fields, uids
 from megacron import api
 
 api.FILE_NAME = './testdb.p'
 
 class TestJobsFunctions(unittest.TestCase):        
 
+
+#### TEST GET_JOBS() ####
+    def test_get_jobs_empty_jobs(self):
+        num_of_jobs = 0
+        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 0 jobs
+        api.set_jobs(self.test_jobs, uids['uid1'])
+        self.jobs_list = api.get_jobs()
+        self.assertEqual(len(self.jobs_list), num_of_jobs) # check that number of jobs is equal to num_of_jobs
+        check_job_fields(self, self.test_jobs, uids['uid1'], self.jobs_list) # verify that the information we get is the same as what was set
+        cleanup()
+
+    def test_get_jobs_one_job(self):
+        num_of_jobs = 1
+        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 1 job
+        api.set_jobs(self.test_jobs, uids['uid1'])
+        self.jobs_list = api.get_jobs()
+        self.assertEqual(len(self.jobs_list), num_of_jobs) # check that number of jobs is equal to num_of_jobs
+        check_job_fields(self, self.test_jobs, uids['uid1'], self.jobs_list) # verify that the information we get is the same as what was set
+        cleanup()
+
+    def test_get_jobs_many_jobs(self):
+        num_of_jobs = 10
+        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 10 jobs
+        api.set_jobs(self.test_jobs, uids['uid1'])
+        self.jobs_list = api.get_jobs()
+        self.assertEqual(len(self.jobs_list), num_of_jobs) # check that number of jobs is equal to num_of_jobs
+        check_job_fields(self, self.test_jobs, uids['uid1'], self.jobs_list) # verify that the information we get is the same as what was set
+        cleanup()
+
+
+#### TEST GET_JOBS_FOR_USER(uid) ####
+    def test_get_jobs_for_user_empty_jobs(self):
+        num_of_jobs = 0
+        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 0 jobs
+        api.set_jobs(self.test_jobs, uids['uid1'])
+        self.jobs_list_uid1 = api.get_jobs_for_user(uids['uid1'])
+        self.assertEqual(len(self.jobs_list_uid1), num_of_jobs) # check that number of jobs for uid1 is equal to num_of_jobs
+        cleanup()
+
+    def test_get_jobs_for_user_many_jobs(self):
+        num_of_jobs = 10
+        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 0 jobs
+        api.set_jobs(self.test_jobs, uids['uid1'])
+        self.jobs_list_uid1 = api.get_jobs_for_user(uids['uid1'])
+        self.assertEqual(len(self.jobs_list_uid1), num_of_jobs) # check that number of jobs for uid1 is equal to num_of_jobs
+        cleanup()
+
+    def test_get_jobs_for_user_multiple(self):
+        num_of_jobs_uid1 = 1
+        self.test_jobs_uid1 = create_test_tab(num_of_jobs_uid1, uids['uid1']) # add 1 job to the tabfile for uid1
+        api.set_jobs(self.test_jobs_uid1, uids['uid1'])
+        self.jobs_list_uid1 = api.get_jobs_for_user(uids['uid1'])
+        num_of_jobs_uid2 = 0
+        self.test_jobs_uid2 = create_test_tab(num_of_jobs_uid2, uids['uid2']) # add 0 jobs to the tabfile for uid2
+        api.set_jobs(self.test_jobs_uid2, uids['uid2'])
+        self.jobs_list_uid2 = api.get_jobs_for_user(uids['uid2'])
+        num_of_jobs_uid3 = 4
+        self.test_jobs_uid3 = create_test_tab(num_of_jobs_uid3, uids['uid3']) # add 4 jobs to the tabfile for uid3
+        api.set_jobs(self.test_jobs_uid3, uids['uid3'])
+        self.jobs_list_uid3 = api.get_jobs_for_user(uids['uid3'])
+        self.assertEqual(len(self.jobs_list_uid1), num_of_jobs_uid1) # check that number of jobs for uid1 is equal to num_of_jobs_uid1
+        self.assertEqual(len(self.jobs_list_uid2), num_of_jobs_uid2) # check that number of jobs for uid2 is equal to num_of_jobs_uid2
+        self.assertEqual(len(self.jobs_list_uid3), num_of_jobs_uid3) # check that number of jobs for uid3 is equal to num_of_jobs_uid3
+        check_job_fields(self, self.test_jobs_uid1, uids['uid1'], self.jobs_list_uid1) # verify that the information we get is the same as what was set
+        check_job_fields(self, self.test_jobs_uid2, uids['uid2'], self.jobs_list_uid2) # verify that the information we get is the same as what was set
+        check_job_fields(self, self.test_jobs_uid3, uids['uid3'], self.jobs_list_uid3) # verify that the information we get is the same as what was set
+        cleanup()
+
+    def test_get_jobs_for_user_none(self):
+        self.jobs_list_uid4 = api.get_jobs_for_user(uids['uid4'])        
+        self.assertTrue(len(self.jobs_list_uid4) == 0) # check that number of jobs for uid4 is equal to 0
+        num_of_jobs_uid1 = 1
+        self.test_jobs_uid1 = create_test_tab(num_of_jobs_uid1, uids['uid1']) # add 1 job to the tabfile for uids['uid1']
+        api.set_jobs(self.test_jobs_uid1, uids['uid1'])
+        self.jobs_list_uid4 = api.get_jobs_for_user(uids['uid4'])
+        self.assertTrue(len(self.jobs_list_uid4) == 0) # check that number of jobs for uid4 is equal to 0
+
+
 #### TEST SET_JOBS([Job], uid) ####
-    def test_set_jobs_none(self):
+    def test_set_jobs_empty_jobs(self):
         num_of_jobs = 0
         checkpoint = datetime.now()
         self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 0 jobs
@@ -23,90 +102,69 @@ class TestJobsFunctions(unittest.TestCase):
         self.assertTrue(new_time > checkpoint) # check that the jobs have been set just now
         cleanup()
 
-    def test_set_jobs_one(self):
-    	num_of_jobs = 1
+    def test_set_jobs_one_job(self):
+        num_of_jobs = 1
         checkpoint = datetime.now()
         self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 1 job
         api.set_jobs(self.test_jobs, uids['uid1'])
-    	db_creation_time = datetime.fromtimestamp(os.stat(api.FILE_NAME).st_mtime)
-    	new_time = db_creation_time.replace(second=db_creation_time.second + 1)
+        db_creation_time = datetime.fromtimestamp(os.stat(api.FILE_NAME).st_mtime)
+        new_time = db_creation_time.replace(second=db_creation_time.second + 1)
         self.assertTrue(new_time > checkpoint) # check that the jobs have been set just now
         cleanup()
 
-    def test_set_jobs_many(self):
+    def test_set_jobs_many_jobs(self):
         num_of_jobs = 10
-    	checkpoint = datetime.now()
+        checkpoint = datetime.now()
         self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 10 jobs
         api.set_jobs(self.test_jobs, uids['uid1'])
-    	db_creation_time = datetime.fromtimestamp(os.stat(api.FILE_NAME).st_mtime)
-    	new_time = db_creation_time.replace(second=db_creation_time.second + 1)
+        db_creation_time = datetime.fromtimestamp(os.stat(api.FILE_NAME).st_mtime)
+        new_time = db_creation_time.replace(second=db_creation_time.second + 1)
         self.assertTrue(new_time > checkpoint) # check that the jobs have been set just now
         cleanup()
 
-#### TEST GET_JOBS() ####
-    def test_get_jobs_none(self):
-        num_of_jobs = 0
-        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 0 jobs
-        api.set_jobs(self.test_jobs, uids['uid1'])
-        self.jobs_list = api.get_jobs()
-        self.assertEqual(len(self.jobs_list), num_of_jobs) # check that number of jobs is equal to num_of_jobs
-        current = test_job_fields(self, self.test_jobs, uids['uid1'], self.jobs_list) # verify that the information we get is the same as what was set
-        cleanup()
 
-    def test_get_jobs_one(self):
+#### TEST SET_JOB_TIME(Job) ####
+    def test_set_job_time_one_job(self):
+        checkpoint1 = datetime.now()
         num_of_jobs = 1
-        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 1 job
-        api.set_jobs(self.test_jobs, uids['uid1'])
-        self.jobs_list = api.get_jobs()
-        self.assertEqual(len(self.jobs_list), num_of_jobs) # check that number of jobs is equal to num_of_jobs
-        current = test_job_fields(self, self.test_jobs, uids['uid1'], self.jobs_list) # verify that the information we get is the same as what was set
-        cleanup()
-
-    def test_get_jobs_many(self):
-        num_of_jobs = 10
-        self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 10 jobs
-        api.set_jobs(self.test_jobs, uids['uid1'])
-        self.jobs_list = api.get_jobs()
-        self.assertEqual(len(self.jobs_list), num_of_jobs) # check that number of jobs is equal to num_of_jobs
-        current = test_job_fields(self, self.test_jobs, uids['uid1'], self.jobs_list) # verify that the information we get is the same as what was set
-        cleanup()
-
-#### TEST GET_JOBS_FOR_USER(uid) ####
-    def test_get_jobs_for_user_empty_jobs(self):
-        num_of_jobs = 0
         self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 0 jobs
         api.set_jobs(self.test_jobs, uids['uid1'])
-        self.jobs_list_uid1 = api.get_jobs_for_user(uids['uid1'])
-        self.assertEqual(len(self.jobs_list_uid1), num_of_jobs) # check that number of jobs for uids['uid1'] is equal to num_of_jobs
+        job = self.test_jobs.pop() # pop the only job in the list to modify its time
+        api.set_job_time(job)
+        checkpoint2 = datetime.now()
+        self.assertTrue(job.last_time_run > checkpoint1 and job.last_time_run < checkpoint2) # check that the job's time was updated correctly
         cleanup()
 
-    def test_get_jobs_for_user_many_jobs(self):
-        num_of_jobs = 10
+    def test_set_job_time_random_job_from_multiple(self):
+        checkpoint1 = datetime.now()
+        num_of_jobs = 3
         self.test_jobs = create_test_tab(num_of_jobs, uids['uid1']) # create a crontab with 0 jobs
         api.set_jobs(self.test_jobs, uids['uid1'])
-        self.jobs_list_uid1 = api.get_jobs_for_user(uids['uid1'])
-        self.assertEqual(len(self.jobs_list_uid1), num_of_jobs) # check that number of jobs for uids['uid1'] is equal to num_of_jobs
+        job = self.test_jobs.pop(random.randrange(len(self.test_jobs))) # pop random job in list to modify its time
+        api.set_job_time(job)
+        checkpoint2 = datetime.now()
+        self.assertTrue(job.last_time_run > checkpoint1 and job.last_time_run < checkpoint2) # check that the job's time was updated correctly
         cleanup()
 
-    def test_get_jobs_for_user_multiple(self):
-        num_of_jobs_uid1 = 1
-        self.test_jobs_uid1 = create_test_tab(num_of_jobs_uid1, uids['uid1']) # add 1 job to the tabfile for uids['uid1']
-        api.set_jobs(self.test_jobs_uid1, uids['uid1'])
-        self.jobs_list_uid1 = api.get_jobs_for_user(uids['uid1'])
-        num_of_jobs_uid2 = 0
-        self.test_jobs_uid2 = create_test_tab(num_of_jobs_uid2, uids['uid2']) # add 0 jobs to the tabfile for uids['uid1']
-        api.set_jobs(self.test_jobs_uid2, uids['uid2'])
-        self.jobs_list_uid2 = api.get_jobs_for_user(uids['uid2'])
-        num_of_jobs_uid3 = 4
-        self.test_jobs_uid3 = create_test_tab(num_of_jobs_uid3, uids['uid3']) # add 4 jobs to the tabfile for uids['uid1']
-        api.set_jobs(self.test_jobs_uid3, uids['uid3'])
-        self.jobs_list_uid3 = api.get_jobs_for_user(uids['uid3'])
-        test_job_fields(self, self.test_jobs_uid1, uids['uid1'], self.jobs_list_uid1) # verify that the information we get is the same as what was set
-        test_job_fields(self, self.test_jobs_uid2, uids['uid2'], self.jobs_list_uid2) # verify that the information we get is the same as what was set
-        test_job_fields(self, self.test_jobs_uid3, uids['uid3'], self.jobs_list_uid3) # verify that the information we get is the same as what was set
-        self.assertEqual(len(self.jobs_list_uid1), num_of_jobs_uid1) # check that number of jobs for uids['uid1'] is equal to num_of_jobs1
-        self.assertEqual(len(self.jobs_list_uid2), num_of_jobs_uid2) # check that number of jobs for uid2 is equal to num_of_jobs2
-        self.assertEqual(len(self.jobs_list_uid3), num_of_jobs_uid3) # check that number of jobs for uid2 is equal to num_of_jobs3
+
+#### TEST CREATE_WORKER() ####
+    def test_create_worker_one(self):
+        num_of_workers = 1
+        workers_list = []
+        checkpoint1 = datetime.now()
+        workers_list.append(api.create_worker()) # create 1 worker and add it to a list for bookkeeping
+        checkpoint2 = datetime.now()
+        check_worker_fields(self, checkpoint1, checkpoint2, workers_list)
+        cleanup()
+
+    def test_create_worker_many(self):
+        num_of_workers = 10
+        workers_list = []
+        checkpoint1 = datetime.now()
+        for job in xrange(num_of_workers):
+            workers_list.append(api.create_worker()) # create 10 workers and add them to a list for bookkeeping
+        checkpoint2 = datetime.now()
+        check_worker_fields(self, checkpoint1, checkpoint2, workers_list)
         cleanup()
 
 if __name__ == '__main__':
